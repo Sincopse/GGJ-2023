c145b0769831279711b0ad6b4d76300e4a4202ef
Francisco Oliveira <franciscoliveira7777@gmail.com>
2023-02-04 16:05:41 +0000

    Update CameraController.cs

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -20,14 +20,9 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(transform.position.x - player.transform.position.x);
-
-        float playerDistance = transform.position.x - player.transform.position.x;
-
-        var playerPosX = playerDistance > limitX ? player.transform.position.x
-                       : playerDistance < -limitX ? player.transform.position.x : transform.position.x;
-
-        playerPosition = new Vector3(playerPosX, player.transform.position.y > 3 ? player.transform.position.y : offsetY, -10);
+        playerPosition = new Vector3(transform.position.x - player.transform.position.x > limitX ? player.transform.position.x
+                                   : transform.position.x - player.transform.position.x < -limitX ? player.transform.position.x
+                                   : transform.position.x, player.transform.position.y > 3 ? player.transform.position.y : offsetY, -10);
         
         transform.position = Vector3.Lerp(transform.position, playerPosition, smoothing * Time.deltaTime);
     }
