cb14b125c304082a64ce88d5e199d8b8e1738eb4
Italo Teofilo Filho <73993878+papitalos@users.noreply.github.com>
2023-02-04 18:26:41 +0000

    oqpsjd

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -24,12 +24,12 @@ public class PlayerMovement2D : MonoBehaviour
         //movimento pulo
         if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x,  jumpingPower * Time.fixedDeltaTime);
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
         if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y * 0.5f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
         Flip();
